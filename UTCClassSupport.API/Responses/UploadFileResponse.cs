020b306d7e36774e9756ac4b6effc8369bd876d8
Clara Phung <clara.phung@developertools.com>
2024-04-13 20:51:31 +0700

    Fix import excel timetable

---
+++ UTCClassSupport.API/Responses/UploadFileResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace UTCClassSupport.API.Responses
+{
+  public class UploadFileResponse : Response
+  {
+    public string FileId { get; set; }
+    public string UrlAccess { get; set; }
+  }
+}
