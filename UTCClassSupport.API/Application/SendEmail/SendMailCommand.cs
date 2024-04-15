cccf3166ebfbb273ccd840ca8e655ed1c0e1fd7d
ClaraPhung <clara.phung@developertools.com>
2024-04-15 16:27:44 +0700

    Default

---
+++ UTCClassSupport.API/Application/SendEmail/SendMailCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using UTCClassSupport.API.Common.Mail;
+using UTCClassSupport.API.Requests;
+using UTCClassSupport.API.Responses;
+
+namespace UTCClassSupport.API.Application.SendEmail
+{
+  public class SendMailCommand : BaseRequest, IRequest<SendMailResponse>
+  {
+    public MailContent Content { get; set; }
+  }
+}
---
+++ UTCClassSupport.API/Application/SendPhoneMessage/SendPhoneMessageCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace UTCClassSupport.API.Application.SendPhoneMessage
+{
+    public class SendPhoneMessageCommand
+    {
+    }
+}
