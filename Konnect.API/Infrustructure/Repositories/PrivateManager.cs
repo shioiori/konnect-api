da7339b07bf29701a648292088cebee1397b8b24
Clara Phung <clara.phung@developertools.com>
2024-06-15 23:07:18 +0700

    Improve security

---
+++ Konnect.API/Infrustructure/Repositories/PrivateManager.cs
@@ -0,0 +1,54 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Text.Json;
+using System.Xml;
+using UTCClassSupport.API.Infrustructure.Data;
+
+namespace Konnect.API.Infrustructure.Repositories
+{
+	public static class PrivateManager
+	{
+		private static Dictionary<string, string> _keys;
+		public static Dictionary<string, string> Keys
+		{
+			get
+			{
+				if (_keys == null)
+				{
+
+					_keys = new Dictionary<string, string>();
+					ReadXml();
+				}
+				return _keys;
+			}
+		}
+		private static void ReadXml()
+		{
+			var path = String.Format(AppDomain.CurrentDomain.BaseDirectory + "\\PrivateKey.xml");
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.Load(path);
+
+			XmlNodeList appSettings = xmlDoc.SelectNodes("/configuration/appSettings/add");
+
+			foreach (XmlNode node in appSettings)
+			{
+				string key = node.Attributes["key"].Value;
+				string value = node.Attributes["value"].Value;
+				_keys.Add(JsonNamingPolicy.CamelCase.ConvertName(key), value);
+			}
+		}
+
+		public class PrivateKey
+		{
+			public const string BaseApiUrl = "BaseApiUrl";
+			public const string BaseChatApiUrl = "BaseChatApiUrl";
+			public const string GoogleApiKey = "GoogleApiKey";
+			public const string GoogleClientId = "GoogleClientId";
+			public const string GoogleDiscoveryDoc = "GoogleDiscoveryDoc";
+			public const string GoogleScopes = "GoogleScopes";
+			public const string JWTTokenName = "JWTTokenName";
+			public const string ImgurClientID = "ImgurClientID";
+			public const string ImgurClientSecret = "ImgurClientSecret";
+		}
+	}
+}
---
+++ Konnect.API/Models/Private.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Konnect.API.Models
+{
+    [Table("private")]
+    public class Private
+    {
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
